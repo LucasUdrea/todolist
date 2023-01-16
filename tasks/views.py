55c60011f0a8eba4554afda5f0a74daeb6f44ab2
Lucas Udrea <lucasudrea@gmail.com>
2023-01-16 12:18:21 -0300

    Changed Forms for Django Forms

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 # List of tasks to do
 tasks = ["foo", "bar", "Jeez"]
 # Create your views here.
@@ -10,4 +13,35 @@ def index (request) :
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+
+    # Check if method is POST
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewTaskForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            task = form.cleaned_data["task"]
+
+            # Add the new task to our list of tasks
+            tasks.append(task)
+
+            # Redirect user to list of tasks
+            return HttpResponseRedirect(reverse("tasks:index"))
+
+        else:
+
+            # If the form is invalid, re-render the page with existing information.
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form" : NewTaskForm()
+    })
+
+class NewTaskForm (forms.Form):
+    task =forms.CharField(label="New Task")
